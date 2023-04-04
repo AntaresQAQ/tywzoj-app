68991a8ae08c1e93aa9b96d0b51b62fc724fad20
Antares <antares.mi@outlook.com>
2023-04-05 04:59:59 +0800

    add env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly TYWZOJ_API_END_POINT: string;
   readonly TYWZOJ_LIGHT_LOGO_URL: string;
   readonly TYWZOJ_DARK_LOGO_URL: string;
+  readonly TYWZOJ_ICON_URL: string;
 }
 
 interface ImportMeta {
